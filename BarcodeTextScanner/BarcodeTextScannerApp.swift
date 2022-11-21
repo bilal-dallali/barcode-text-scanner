05f002afdcf1a25fae2c91034b1d0e8de26b392f
bilal-webdeveloper <dallali-bilal@hotmail.fr>
2022-11-21 22:38:58 +0100

    Initial Commit

---
+++ BarcodeTextScanner/BarcodeTextScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeTextScannerApp.swift
+//  BarcodeTextScanner
+//
+//  Created by Bilal Dallali on 21/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeTextScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
